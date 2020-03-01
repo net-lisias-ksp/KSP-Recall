d90ca5e07c7f4b17ff3ba5096bbd445e942782f6
Lisias <me@lisias.net>
2020-03-01 04:11:50 -0300

    YEAH!! https://github.com/net-lisias-ksp/TweakScale/issues/96 is fixed!!!! :)

--- Source/Resourceful/PartModule.cs
+++ Source/Resourceful/PartModule.cs
@@ -21,35 +21,109 @@
 
 */
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace KSP_Recall
 
 {
 	public class Resourceful : PartModule
 	{
+
+		#region KSP UI
+
 		[KSPField(isPersistant = false, guiActiveEditor = true, guiName = "KSP-Recall::Resourceful")]//Scale
 		[UI_Toggle(disabledText = "Disabled", enabledText = "Enabled", scene = UI_Scene.Editor)]
 		public bool active = false;
+
+		#endregion
+
+
+		[Serializable]
+		private struct Resource_t
+		{
+			[SerializeField] private PartResource node;
+
+			internal PartResource ToPartResource(Part part)
+			{
+				PartResource r = new PartResource(part);
+				r.Copy(node);
+				return r;
+			}
+
+			internal static Resource_t From(PartResource pr)
+			{
+				Resource_t r = new Resource_t();
+				r.node = pr;
+				return r;
+			}
+		}
+
+		[Serializable]
+		private class Resource_List : ScriptableObject
+		{
+			private List<Resource_t> list = null;
+			public List<Resource_t> List => this.list ?? (this.list = new List<Resource_t>());
+		}
+
+		[SerializeField]
+		private Resource_List resource;
+
 
 		#region KSP Events
 
 		public override void OnAwake()
 		{
 			Log.dbg("OnAwake {0}", this.name);
+			base.OnAwake();
+			if (null == this.resource) this.resource = new Resource_List();
 		}
 
 		public override void OnStart(StartState state)
 		{
-			Log.dbg("OnStart {0} {1}", this.name, this.active);
+			Log.dbg("OnStart {0} {1} {2}", this.name, state, this.active);
+			base.OnStart(state);
+			if (0 != this.resource.List.Count) this.RestoreResourceList();
 		}
 
 		public override void OnLoad(ConfigNode node)
 		{
 			Log.dbg("OnLoad {0} {1}", this.name, null != node);
+			base.OnLoad(node);
 		}
 
 		#endregion
 
+		#region Part Events Handlers
+		[KSPEvent(guiActive = false, active = true)]
+		void OnPartScaleChanged(BaseEventDetails data)
+		{
+			// Just to validate the package
+			float factorAbsolute = data.Get<float>("factorAbsolute");
+			float factorRelative = data.Get<float>("factorRelative");
+
+			Log.dbg("OnPartScaleChanged {0} {1}", factorAbsolute, factorRelative);
+
+			this.UpdateResourceList();
+		}
+		#endregion
+
+		private void UpdateResourceList()
+		{
+			this.resource.List.Clear();
+			foreach (PartResource pr in this.part.Resources)
+				this.resource.List.Add(Resource_t.From(pr));
+			Log.dbg("Updated {0} resources for {1}", this.resource.List.Count, this.name);
+		}
+
+		private void RestoreResourceList()
+		{
+			Log.dbg("Restoring {0} resources from {1}", this.resource.List.Count, this.name);
+
+			this.part.Resources.Clear();
+			foreach (Resource_t resource in this.resource.List)
+				this.part.Resources.Add(resource.ToPartResource(this.part));
+		}
 
 		private static KSPe.Util.Log.Logger Log = KSPe.Util.Log.Logger.CreateForType<Resourceful>("KSP-Recall", "Resourceful");
 		static Resourceful()
