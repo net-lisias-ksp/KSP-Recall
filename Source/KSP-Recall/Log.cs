ed46568819283741cd96f8867c59718bc9ab1d9e
Lisias <me@lisias.net>
2020-05-17 17:42:05 -0300

    Some logging adjustments

--- Source/KSP-Recall/Log.cs
+++ Source/KSP-Recall/Log.cs
@@ -23,70 +23,70 @@ using System.Collections.Generic;
 
 namespace KSP_Recall
 {
-    public static class Log
-    {
-        private static readonly Logger log = Logger.CreateForType<Startup>();
+	public static class Log
+	{
+		private static readonly Logger log = Logger.CreateForType<Startup>();
 
-        internal static void init()
-        {
-            log.level =
+		internal static void init()
+		{
+			log.level =
 #if DEBUG
-                Level.TRACE
+				Level.TRACE
 #else
                 Level.INFO
 #endif
-                ;
-        }
+				;
+		}
 
-        internal static void force (string msg, params object [] @params)
-        {
-            log.force (msg, @params);
-        }
+		internal static void force(string msg, params object[] @params)
+		{
+			log.force(msg, @params);
+		}
 
-        internal static void info(string msg, params object[] @params)
-        {
-            log.info(msg, @params);
-        }
+		internal static void info(string msg, params object[] @params)
+		{
+			log.info(msg, @params);
+		}
 
-        internal static void warn(string msg, params object[] @params)
-        {
-            log.warn(msg, @params);
-        }
+		internal static void warn(string msg, params object[] @params)
+		{
+			log.warn(msg, @params);
+		}
 
-        internal static void detail(string msg, params object[] @params)
-        {
-            log.detail(msg, @params);
-        }
+		internal static void detail(string msg, params object[] @params)
+		{
+			log.detail(msg, @params);
+		}
 
-        internal static void error(Exception e, object offended)
-        {
-            log.error(offended, e);
-        }
+		internal static void error(Exception e, object offended)
+		{
+			log.error(offended, e);
+		}
 
-        internal static void error(string msg, params object[] @params)
-        {
-            log.error(msg, @params);
-        }
+		internal static void error(string msg, params object[] @params)
+		{
+			log.error(msg, @params);
+		}
 
-        [ConditionalAttribute("DEBUG")]
-        internal static void dbg(string msg, params object[] @params)
-        {
-            log.trace(msg, @params);
-        }
+		[ConditionalAttribute("DEBUG")]
+		internal static void dbg(string msg, params object[] @params)
+		{
+			log.trace(msg, @params);
+		}
 
-        #if DEBUG
-        private static readonly HashSet<string> DBG_SET = new HashSet<string>();
-        #endif
+#if DEBUG
+		private static readonly HashSet<string> DBG_SET = new HashSet<string>();
+#endif
 
-        [ConditionalAttribute("DEBUG")]
-        internal static void dbgOnce(string msg, params object[] @params)
-        {
-            string new_msg = string.Format(msg, @params);
-            #if DEBUG
-            if (DBG_SET.Contains(new_msg)) return;
-            DBG_SET.Add(new_msg);
-            #endif
-            log.trace(new_msg);
-        }
-    }
+		[ConditionalAttribute("DEBUG")]
+		internal static void dbgOnce(string msg, params object[] @params)
+		{
+			string new_msg = string.Format(msg, @params);
+#if DEBUG
+			if (DBG_SET.Contains(new_msg)) return;
+			DBG_SET.Add(new_msg);
+#endif
+			log.trace(new_msg);
+		}
+	}
 }
