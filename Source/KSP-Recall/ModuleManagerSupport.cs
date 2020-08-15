d1211fb05f72ec8ab91278774dd4cad7a6bb4305
Lisias <me@lisias.net>
2020-08-15 16:22:52 -0300

    WAY smarter way to prevent the loading of the work arounds when not needed!

---
+++ Source/KSP-Recall/ModuleManagerSupport.cs
@@ -0,0 +1,46 @@
+﻿/*
+	This file is part of KSP-Recall
+	(C) 2020 Lisias T : http://lisias.net <support@lisias.net>
+
+	KSP-Recall is double licensed, as follows:
+
+	* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+	* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	KSP-Recall is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with KSP-Recall. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with KSP-Recall. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+using System.Collections.Generic;
+
+namespace KSP_Recall
+{
+	public static class ModuleManagerSupport
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			List<string> tags = new List<string>();
+
+			if (checkForResourceful()) tags.Add("KSPRECALL-RESOURCEFUL");
+
+			return tags.ToArray();
+		}
+
+		private static bool checkForResourceful()
+		{
+			if (1 == KSPe.Util.KSP.Version.Current.MAJOR && 9 == KSPe.Util.KSP.Version.Current.MINOR) return true;
+
+			return false;
+		}
+	}
+}
