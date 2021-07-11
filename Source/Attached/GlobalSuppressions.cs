64896f58a227e3f81c9a3bfa3e748b9c2d228f58
Lisias T <me@lisias.net>
2021-07-11 20:03:46 -0300

    Project Sanitizing

---
+++ Source/Attached/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0018:Comparison of floating point numbers with equality operator", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Attached.Pool.Attachment_t.Equals(AttachNode)~System.Boolean")]
---
+++ Source/Refunding/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0082:Parameter has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.AsynchronousUpdate(System.Int32)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0117:Local variable has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.UpdateResource")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0117:Local variable has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.RemoveResourceIfAvailable")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0082:Parameter has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.CalculateResourceCost(ProtoPartResourceSnapshot)~System.Double")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0082:Parameter has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.CalculateResourceCost(PartResource)~System.Double")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0117:Local variable has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.CalculateResourcesCost~System.Double")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0117:Local variable has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.IsStackable~System.Boolean")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0117:Local variable has the same name as a member and hides it", Justification = "<Pending>", Scope = "member", Target = "~M:KSP_Recall.Refunds.Refunding.RestoreResource~PartResource")]
